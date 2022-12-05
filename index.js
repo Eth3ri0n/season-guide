30f12df4019db4702eb7b60d12695df2ba511a05
Eth3ri0n <105993207+Eth3ri0n@users.noreply.github.com>
2022-12-05 03:44:56 +0100

    New update  index.js file

--- index.js
+++ index.js
@@ -1,103 +1,123 @@
-/** Navbar menu */
+// Déclaration d'objet
+const Menu_Items = {
+  Logo_Light_Theme: "./src/img/logo-l.PNG",
+  Logo_Dark_Theme: "./src/img/logo-d.PNG",
+  Home: "Accueil",
+  FL: "Fruits & Légumes",
+  About: "À propos",
+  Mentions: "Mentions Légales",
+};
+
+// Compsant Menu
+const Menu_Component = `
+      <a href="./index.html"><img src="${Menu_Items.Logo_Light_Theme}" alt="Logo" /></a>
+      <nav class="navbar">
+        <ul class="menu">
+          <li class="menu-list">
+            <a href="./index.html"
+              ><img src="./src/img/home.PNG" alt="Home icon" class="icon-size"/><span>${Menu_Items.Home}</span></a
+            >
+          </li>
+          <li class="menu-list">
+            <a href="./fruits_legumes.html"
+              ><img src="./src/img/fl.PNG" alt="Fruits and Vegetable" class="icon-size"/><span>${Menu_Items.FL}</span></a
+            >
+          </li>
+          <li class="menu-list">
+            <a href="./about.html"
+              ><img src="./src/img/about.PNG" alt="About icon" class="icon-size"/><span
+                >${Menu_Items.About}</span
+              ></a
+            >
+          </li>
+        </ul>
+      </nav>
+      <div class="burger">
+        <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span>
+      </div>
+      <i class="btn-theme fa fa-sun"></i>
+`;
+
+// Injection dans le container de class="nav"
+const Nav_Menu = document.querySelector(".nav");
+Nav_Menu.insertAdjacentHTML("afterbegin", Menu_Component);
+
+/** Navbar Burger menu */
 let burger = document.querySelector(".burger");
 burger.addEventListener("click", function () {
   let navbar = document.querySelector(".navbar");
   navbar.classList.toggle("active");
 });
 
-/** Flip Card Game **/
-const cards = document.querySelectorAll(".card"),
-  timeTag = document.querySelector(".time b"),
-  flipsTag = document.querySelector(".flips b"),
-  refreshBtn = document.querySelector(".details button");
+// Theme Mode
+const Theme_Mode = document.querySelector(".btn-theme");
+Theme_Mode.addEventListener("click", () => {
+  const body = document.body;
 
-let maxTime = 30;
-let timeLeft = maxTime;
-let flips = 0;
-let matchedCard = 0;
-let disableDeck = false;
-let isPlaying = false;
-let cardOne, cardTwo, timer;
-
-function initTimer() {
-  if (timeLeft <= 0) {
-    return clearInterval(timer);
+  if (
+    body.classList.contains("dark") &&
+    Theme_Mode.classList.contains("fa-moon")
+  ) {
+    body.classList.add("light");
+    Theme_Mode.classList.add("fa-sun");
+    body.classList.remove("dark");
+    Theme_Mode.classList.remove("fa-moon");
+  } else if (
+    body.classList.contains("light") &&
+    Theme_Mode.classList.contains("fa-sun")
+  ) {
+    body.classList.add("dark");
+    Theme_Mode.classList.add("fa-moon");
+    body.classList.remove("light");
+    Theme_Mode.classList.remove("fa-sun");
   }
-  timeLeft--;
-  timeTag.innerText = timeLeft;
-}
+});
 
-function flipCard({ target: clickedCard }) {
-  if (!isPlaying) {
-    isPlaying = true;
-    timer = setInterval(initTimer, 1000);
-  }
-  if (clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
-    flips++;
-    flipsTag.innerText = flips;
-    clickedCard.classList.add("flip");
-    if (!cardOne) {
-      return (cardOne = clickedCard);
-    }
-    cardTwo = clickedCard;
-    disableDeck = true;
-    let cardOneImg = cardOne.querySelector(".back-view img").src,
-      cardTwoImg = cardTwo.querySelector(".back-view img").src;
-    matchCards(cardOneImg, cardTwoImg);
-  }
-}
+// Listes des fruits & légumes
+function SetupTabs() {
+  document.querySelectorAll(".tabs_button").forEach((button) => {
+    button.addEventListener("click", () => {
+      const SideBar = button.parentElement;
+      const TabsContainer = SideBar.parentElement;
+      const TabNumber = button.dataset.forTab;
+      const TabToActivate = TabsContainer.querySelector(
+        `.tabs_content[data-tab="${TabNumber}"]`
+      );
 
-function matchCards(img1, img2) {
-  if (img1 === img2) {
-    matchedCard++;
-    if (matchedCard == 4 && timeLeft > 0) {
-      return clearInterval(timer);
-    }
-    cardOne.removeEventListener("click", flipCard);
-    cardTwo.removeEventListener("click", flipCard);
-    cardOne = cardTwo = "";
-    return (disableDeck = false);
-  }
+      SideBar.querySelectorAll(".tabs_button").forEach((button) => {
+        button.classList.remove("tabs_button--active");
+      });
 
-  setTimeout(() => {
-    cardOne.classList.add("shake");
-    cardTwo.classList.add("shake");
-  }, 400);
+      TabsContainer.querySelectorAll(".tabs_content").forEach((tab) => {
+        tab.classList.remove("tabs_content--active");
+      });
 
-  setTimeout(() => {
-    cardOne.classList.remove("shake", "flip");
-    cardTwo.classList.remove("shake", "flip");
-    cardOne = cardTwo = "";
-    disableDeck = false;
-  }, 1200);
-}
-
-function shuffleCard() {
-  timeLeft = maxTime;
-  flips = matchedCard = 0;
-  cardOne = cardTwo = "";
-  clearInterval(timer);
-  timeTag.innerText = timeLeft;
-  flipsTag.innerText = flips;
-  disableDeck = isPlaying = false;
-
-  let arr = [1, 2, 3, 4, 1, 2, 3, 4];
-  arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
-
-  cards.forEach((card, index) => {
-    card.classList.remove("flip");
-    let imgTag = card.querySelector(".back-view img");
-    setTimeout(() => {
-      imgTag.src = `img/img-${arr[index]}.PNG`;
-    }, 500);
-    card.addEventListener("click", flipCard);
+      button.classList.add("tabs_button--active");
+      TabToActivate.classList.add("tabs_content--active");
+    });
   });
 }
+document.addEventListener("DOMContentLoaded", () => {
+  SetupTabs();
 
-shuffleCard();
-
-refreshBtn.addEventListener("click", shuffleCard);
-
-cards.forEach((card) => {
-  card.addEventListener("click", flipCard);
+  document.querySelectorAll(".tabs").forEach((TabsContainer) => {
+    TabsContainer.querySelector(".tabs_sidebar .tabs_button").click();
+  });
 });
+
+// Composant Footer
+const Footer_Component = `
+  <div class="footer-info">
+    <img src="${Menu_Items.Logo_Light_Theme}" alt="logo" />
+    <ul>
+      <li><a href="./mentions-legales.html">Mentions légales</a></li>
+    </ul>
+  </div>
+  <span class="copy">&copy; Season Guide, all right reseved | 2022</span>
+`;
+
+// Injection dans le container de class="nav"
+const Footer_Menu = document.querySelector("#footer");
+Footer_Menu.insertAdjacentHTML("beforeend", Footer_Component);
